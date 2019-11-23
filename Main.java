3e0afb8c5d07e9c4c25d0f666e8525e1e130867b
Vany <vany8462@gmail.com>
2019-11-23 15:27:44 +0800

    create window of the game

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
 package com.vany;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    JFrame frame = new JFrame("Block Breaker");
+
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	    frame.setVisible(true);
+	    frame.setSize(490,600);
     }
 }
