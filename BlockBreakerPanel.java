31c8d7347b98044308978dfab6307149ee847642
Vany <vany8462@gmail.com>
2019-11-24 13:33:04 +0800

    make extra balls rebound when intersect with paddle

--- BlockBreakerPanel.java
+++ BlockBreakerPanel.java
@@ -59,6 +59,10 @@ public class BlockBreakerPanel extends JPanel implements KeyListener {
     public void update(){
         for(Block p : powerup){
             p.y+=1;
+            if(p.intersects(paddle) && !p.destroyed){
+                p.destroyed = true;
+                ball.add(new Block(paddle.dx+75, 437, 25, 25, "ball.png"));
+            }
         }
         for(Block ba : ball){
             ba.x += ba.dx;
